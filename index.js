b351fb051c1ce576e449eded6f94309456b9eeba
QuickSilver <hi.sadaqatali@gmail.com>
2024-09-11 22:12:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 var toggleButton = document.getElementById('toggleButton');
 var skillsDiv = document.getElementById('skills');
+
 if (toggleButton && skillsDiv) {
     toggleButton.addEventListener('click', function () {
         if (skillsDiv.style.display === 'none') {
             skillsDiv.style.display = 'block';
-            toggleButton.innerText = "Hide Skills"; // Use innerText or textContent for button text
-        }
-        else {
+            toggleButton.value = "Hide Skills";
+        } else {
             skillsDiv.style.display = 'none';
-            toggleButton.innerText = "Show Skills";
+            toggleButton.value = "Show Skills";
         }
     });
-}
-else {
-    console.error("Required element not found in the DOM");
 }
