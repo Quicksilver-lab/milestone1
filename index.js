7289111bfda118a37f0584004dd61e12b2baa902
QuickSilver <hi.sadaqatali@gmail.com>
2024-09-11 22:27:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // Function to open tabs
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
+    
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
+
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+    
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-// Default to show the first tab
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementsByClassName("tablinks")[0].click();
 });
